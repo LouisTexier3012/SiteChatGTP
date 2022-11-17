e3de6f6dfcd7bd43df07f3ae28f1dc295b767d33
Quentin RIOS-SERRA <quentin.rios-serra@etu.umontpellier.fr>
2022-11-17 11:27:52 +0100

    TD6:6.2 wip

--- TD6/web/frontController.php
+++ TD6/web/frontController.php
@@ -8,10 +8,13 @@ $loader->addNamespace('App\Covoiturage', __DIR__ . '/../src'); // register the b
 $loader->register(); // register the autoloader
 
 // On recupère l'action passée dans l'URL
-if (!isset($_GET["action"])) $action = "readAll";
-else                         $action = $_GET["action"];
+if (isset($_GET["action"])) $action = $_GET["action"];
+else						$action = "readAll";
 
 $isMethod = in_array($action, get_class_methods("App\Covoiturage\Controller\ControllerVoiture"));
 if ($isMethod) ControllerVoiture::$action(); // Appel de la méthode statique $action de ControllerVoiture
 else           ControllerVoiture::error("L'action n'existe pas");
+
+if (isset($_GET["controller"])) $controller = $_GET['controller'];
+else							$controller = "voiture";
 ?>
